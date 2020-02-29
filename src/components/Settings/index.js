f0c940b93b0279c2b60555afa50244606bf5942c
Durgaprasad Budhwani <durgaprasad.budhwani@gmail.com>
2020-02-29 12:54:23 +0530

    - Fixed in position

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -36,20 +36,6 @@ const WorkspaceForm = ({ saveSettingsData, updateFormValues, settings }) => (
                 <Grid item xs={12}>
                     <Box mb={2}>
                         <TextField
-                            value={settings.client_secret || ''}
-                            onChange={(e) => updateFormValues({ client_secret: e.target.value })}
-                            label="Client Secret"
-                            fullWidth
-                            variant="outlined"
-                        />
-                        <Typography variant="caption">
-                            This is Client Secret
-                        </Typography>
-                    </Box>
-                </Grid>
-                <Grid item xs={12}>
-                    <Box mb={2}>
-                        <TextField
                             value={settings.url || ''}
                             onChange={(e) => updateFormValues({ url: e.target.value })}
                             label="URL"
@@ -72,6 +58,20 @@ const WorkspaceForm = ({ saveSettingsData, updateFormValues, settings }) => (
                         />
                         <Typography variant="caption">
                             This is Client ID
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12}>
+                    <Box mb={2}>
+                        <TextField
+                            value={settings.client_secret || ''}
+                            onChange={(e) => updateFormValues({ client_secret: e.target.value })}
+                            label="Client Secret"
+                            fullWidth
+                            variant="outlined"
+                        />
+                        <Typography variant="caption">
+                            This is Client Secret
                         </Typography>
                     </Box>
                 </Grid>
